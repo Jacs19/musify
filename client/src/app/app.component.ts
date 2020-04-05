53ab7de473f42f4ec0c7cccd315a432bfa9f92b9
Jhonatan Chacón <jacspro@gmail.com>
2020-04-05 16:16:28 -0400

    se finaliza manejo de formulario para iniciar sesion

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent implements OnInit {
   public user: User;
   public identity;
   public token;
+  public errorMessage;
 
   constructor(
     private _userService: UserService
@@ -22,22 +23,69 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(){
-    
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
   }
 
   public onSubmit(){
     console.log(this.user);
 
+    //se obtienen los datos del usuario
     this._userService.signup(this.user).subscribe(
       response => {
-        console.log(response);
+        let identity = response['user'];
+        this.identity = identity;
+        
+        if(!this.identity._id){
+          alert("El usuario no esta correctamente identificado");
+        }else{
+          //se crea sesion en local storage, para guardar los datos del usuario
+          localStorage.setItem('identity', JSON.stringify(identity));
+
+          //se obtiene el token del usuario, para poder agregarlo a cada peticion
+          this._userService.signup(this.user, 'true').subscribe(
+            response => {
+              let token = response['token'];
+              this.token = token;
+
+              if(this.token <= 0){
+                alert("El token no se ha generado correctamente");
+              }else{
+                localStorage.setItem('token', token);
+
+                console.log(token);
+                console.log(identity);
+              }
+            },
+            error => {
+              var errorMessage = <any>error;
+              if(errorMessage != null){
+                //var body = JSON.parse(error._body);
+                this.errorMessage = errorMessage.error.mensaje;
+                console.log(errorMessage);
+              }
+            }
+          );
+        }
       },
       error => {
         var errorMessage = <any>error;
         if(errorMessage != null){
-          console.log(error);
+          //var body = JSON.parse(error._body);
+          this.errorMessage = errorMessage.error.mensaje;
+          console.log(errorMessage);
         }
       }
     );
   }
+
+  logout(){
+    localStorage.removeItem('identity');
+    localStorage.removeItem('token');
+    //elimina por completo los datos guardados en el local storage
+    localStorage.clear();
+    this.identity = null;
+    this.token = null;
+  }
+
 }
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse, HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 //import { Observable } from 'rxjs/Observable';
 import { GLOBAL } from './global';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Injectable()
 export class UserService{
 
     public url: string;
+    public identity;
+    public token;
 
     constructor(private _http: HttpClient){
         this.url = GLOBAL.url;
@@ -18,12 +21,40 @@ export class UserService{
             user_to_login.gethash = gethash;
         }
         let json = JSON.stringify(user_to_login);
-        let params = json;
+        let body = json;
 
         let headers = new HttpHeaders({'Content-Type': 'application/json'});
 
-        return this._http.post(this.url+'login', params, 
-                                {headers: headers}).pipe(map(res => res.json()));
+        //return this._http.post(this.url+'login', params, {headers: headers}).pipe(map(res => res));
+
+        return this._http.post(this.url + "login", body, {
+            headers: headers,
+            responseType: 'json'
+        });
+    }
+
+    getIdentity(){
+        let identity = JSON.parse(localStorage.getItem('identity'));
+        
+        if(identity != undefined){
+            this.identity = identity;
+        }else{
+            this.identity = null;
+        }
+        
+        return this.identity;
+    }
+
+    getToken(){
+        let token = JSON.parse(localStorage.getItem('token'));
+        
+        if(token != undefined){
+            this.token = this.token;
+        }else{
+            this.token = null;
+        }
+
+        return this.token;
     }
 
 }
